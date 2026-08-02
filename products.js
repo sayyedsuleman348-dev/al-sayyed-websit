b15750965767edf333591112ebf37eeebc758286
sayyedsuleman348-dev <sayyedsuleman348@gmail.com>
2026-08-02 15:42:31 +0530

    Add files via upload

---
+++ products.js
@@ -0,0 +1,318 @@
+const products = [
+  {
+    id: 1,
+    name: "Rose 0ud",
+    price: 129,
+    image: "rose-attar.jpg",
+    category: "Attar"
+  },
+  {
+    id: 2,
+    name: "Champion 30ml",
+    price: 200,
+    image: "champion.jpg",
+    category: "Perfume"
+  },
+  {
+    id: 3,
+    name: "golden oud",
+    price: 99,
+    image: "solid.jpg",
+    category: "Solid Perfume"
+  },
+  {
+    id: 4,
+    name: "Bakhoor 50gm",
+    price: 129,
+    image:"bakhoor.jpg",
+category:"Bakhoor"
+  },
+  {
+  id: 5,
+  name: "Golden Oud",
+  price: 129,
+  image: "Goldenoud.jpg",
+  category: "Attar"
+  },
+  {
+  id: 6,
+  name: "ameer ul Oud",
+  price: 119,
+  image: "Ameeruloud.jpg",
+  category: "Attar"
+  },
+  {
+  id: 7,
+  name: "turkis Oud",
+  price: 100,
+  image: "turkisoud.jpg",
+  category: "Attar"
+  },
+   {
+  id: 8,
+  name: "midnight Oud",
+  price: 110,
+  image: "attar1.jpg",
+  category: "Attar"
+  },
+  {
+  id: 9,
+  name: "choco Oud",
+  price: 110,
+  image: "attar1.jpg",
+  category: "Attar"
+  },
+  {
+  id: 10,
+  name: "weekend",
+  price: 129,
+  image: "Attar1.jpg",
+  category: "Attar"
+  },
+  {
+  id: 11,
+  name: "lavander Oud",
+  price: 99,
+  image: "Attar1.jpg",
+  category: "Attar"
+  },
+  {
+  id: 12,
+  name: "white musk",
+  price: 89,
+  image: "Attar1.jpg",
+  category: "Attar"
+  },
+  {
+  id: 13,
+  name: "rose musk",
+  price: 89,
+  image: "Attar1.jpg",
+  category: "Attar"
+  },
+  {
+  id: 14,
+  name: "assel",
+  price: 90,
+  image: "Attar1.jpg",
+  category: "Attar"
+  },
+  {
+  id: 15,
+  name: "assel amber styel",
+  price: 99,
+  image: "Attar1.jpg",
+  category: "Attar"
+  },
+  {
+  id: 16,
+  name: "arabic",
+  price: 110,
+  image: "Attar1.jpg",
+  category: "Attar"
+  },
+  {
+  id: 17,
+  name: "al jhara",
+  price: 99,
+  image: "Attar1.jpg",
+  category: "Attar"
+  },
+  {
+  id: 18,
+  name: "bule sultan",
+  price: 119,
+  image: "Attar1.jpg",
+  category: "Attar"
+  },
+  {
+  id: 19,
+  name: "choco lava",
+  price: 99,
+  image: "Attar1.jpg",
+  category: "Attar"
+  },
+  {
+  id: 20,
+  name: "champion",
+  price: 110,
+  image: "Attar1.jpg",
+  category: "Attar"
+  },
+  {
+  id: 21,
+  name: "lavander",
+  price: 89,
+  image: "Attar1.jpg",
+  category: "Attar"
+  },
+  {
+  id: 22,
+  name: "mogra",
+  price: 89,
+  image: "Attar1.jpg",
+  category: "Attar"
+  },
+  {
+  id: 23,
+  name: "rose",
+  price:  89,
+  image: "Attar1.jpg",
+  category: "Attar"
+  },
+  {
+    id: 24,
+    name: "charcol Bakhoor 50gm",
+    price: 139,
+    image:"bakhoor.jpg",
+category:"Bakhoor"
+  },
+  {
+    id: 25,
+    name: "majestic Bakhoor 50gm",
+    price: 119,
+    image:"bakhoor.jpg",
+category:"Bakhoor"
+  },
+  {
+    id: 26,
+    name: "white oud",
+    price: 99,
+    image: "solid.jpg",
+    category: "Solid Perfume"
+  },
+    {
+    id: 27,
+    name: "zara",
+    price: 99,
+    image: "solid.jpg",
+    category: "Solid Perfume"
+  },
+  {
+    id: 28,
+    name: "sport elite",
+    price: 99,
+    image: "solid.jpg",
+    category: "Solid Perfume"
+  },
+   {
+    id: 29,
+    name: "weekend 30ml",
+    price: 219,
+    image: "champion.jpg",
+    category: "Perfume"
+  },
+  {
+    id: 30,
+    name: "bule sultan 30ml",
+    price: 219,
+    image: "champion.jpg",
+    category: "Perfume"
+  },
+  {
+    id: 31,
+    name: "choco lava 30ml",
+    price: 200,
+    image: "champion.jpg",
+    category: "Perfume"
+  },
+   {
+    id: 32,
+    name: "ameer ul od 30ml",
+    price: 219,
+    image: "champion.jpg",
+    category: "Perfume"
+  },
+  {
+    id: 33,
+    name: "assel 30ml",
+    price: 200,
+    image: "champion.jpg",
+    category: "Perfume"
+  },
+  {
+    id: 34,
+    name: "al jhara 30ml",
+    price: 200,
+    image: "champion.jpg",
+    category: "Perfume"
+  },
+  {
+    id: 35,
+    name: "midnight oud 30ml",
+    price: 200,
+    image: "champion.jpg",
+    category: "Perfume"
+  },
+  {
+    id: 36,
+    name: "turkis oud 30ml",
+    price: 220,
+    image: "champion.jpg",
+    category: "Perfume"
+  },
+  {
+    id: 37,
+    name: "royal rose 30ml",
+    price: 200,
+    image: "champion.jpg",
+    category: "Perfume"
+  },
+  {
+    id: 38,
+    name: "weekend 20ml",
+    price: 149,
+    image: "20ml.jpg",
+    category: "Perfume"
+  },
+  {
+    id: 39,
+    name: "al jhara 20ml",
+    price: 149,
+    image: "20ml.jpg",
+    category: "Perfume"
+  },
+  {
+    id: 40,
+    name: "champion 20ml",
+    price: 149,
+    image: "20ml.jpg",
+    category: "Perfume"
+  },
+  {
+    id: 41,
+    name: "bule sultan 20ml",
+    price: 149,
+    image: "20ml.jpg",
+    category: "Perfume"
+  },
+  {
+    id: 42,
+    name: "chocolava 20ml",
+    price: 149,
+    image: "20ml.jpg",
+    category: "Perfume"
+  },
+  {
+    id: 43,
+    name: "turkis oud 20ml",
+    price: 149,
+    image: "20ml.jpg",
+    category: "Perfume"
+  },
+  {
+    id: 44,
+    name: "pack of 4 each 20ml (aljhara ,royalrose,weekend,chocolava ",
+    price: 449,
+    image: "pack1.jpg",
+    category: "Perfume"
+  },
+   {
+    id: 45,
+    name: "musk e tahara ",
+    price: 100,
+    image: "musketahara.jpg",
+    category: "Attar"
+  },
+
+];
